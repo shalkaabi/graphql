7808ad3ef9d556a9148ac773d911d78579edd9b1
shalkaabi <shaikha0alkaabi@gmail.com>
2026-04-26 13:36:19 +0300

    Fix AUTH_URL to point to learn.reboot01.com API

--- Js/graphql.js
+++ Js/graphql.js
@@ -1,5 +1,5 @@
 const API_DOMAIN = 'learn.reboot01.com';
-const AUTH_URL = `https://shalkaabi.github.io/graphql/`;
+const AUTH_URL = `https://${API_DOMAIN}/api/auth/signin`;
 const GRAPHQL_URL = `https://learn.reboot01.com/api/graphql-engine/v1/graphql`;
 
 function parseJwt(token) {
@@ -55,4 +55,3 @@ function setupLogout() {
     window.location.href = 'index.html';
   });
 }
-
